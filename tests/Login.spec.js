cbb476286444221dcd27c5cbd730f97df6582395
Jitkal Mitrakul <66130500113@ad.sit.kmutt.ac.th>
2025-11-12 15:21:23 +0700

    test: add password error case for Facebook login flow

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -12,3 +12,64 @@ test('case1 : Normal Login', async ({ page }) => {
 
 });
 
+test('case2 : Login with incorrect password', async ({ page }) => {
+
+  await page.goto('https://www.facebook.com/');
+  await page.locator("xpath=//input[@id='email']").fill('main.test.automate@gmail.com')
+  await page.locator("xpath=//input[@id='pass']").fill('Password_0')
+  await page.getByRole('button', { name: 'Log in' }).click()
+
+  await expect(page.getByText("The password that you've entered is incorrect." )).toHaveCount(1);
+});
+
+test('case3 : Login with true password but wrong language', async ({ page }) => {
+
+  await page.goto('https://www.facebook.com/');
+  await page.locator("xpath=//input[@id='email']").fill('main.test.automate@gmail.com')
+  await page.locator("xpath=//input[@id='pass']").fill('ญฟหหไนพก๘ๅ')
+  await page.getByRole('button', { name: 'Log in' }).click()
+
+  await expect(page.getByText("The password that you've entered is incorrect." )).toHaveCount(1);
+});
+
+// facebook error
+test('case4 : Login with true password but have 1 spacebar at front', async ({ page }) => {
+
+  await page.goto('https://www.facebook.com/');
+  await page.locator("xpath=//input[@id='email']").fill('main.test.automate@gmail.com')
+  await page.locator("xpath=//input[@id='pass']").fill(' Password_1')
+  await page.getByRole('button', { name: 'Log in' }).click()
+
+  await expect(page.getByText("The password that you've entered is incorrect." )).toHaveCount(1);
+});
+
+test('case5 : Login with true password but have exceed spacebar at front', async ({ page }) => {
+
+  await page.goto('https://www.facebook.com/');
+  await page.locator("xpath=//input[@id='email']").fill('main.test.automate@gmail.com')
+  await page.locator("xpath=//input[@id='pass']").fill('    Password_1')
+  await page.getByRole('button', { name: 'Log in' }).click()
+
+  await expect(page.getByText("The password that you've entered is incorrect." )).toHaveCount(1);
+});
+
+
+test('case6 : Login with true password but have exceed spacebar in the middle', async ({ page }) => {
+
+  await page.goto('https://www.facebook.com/');
+  await page.locator("xpath=//input[@id='email']").fill('main.test.automate@gmail.com')
+  await page.locator("xpath=//input[@id='pass']").fill('Pass  word_1')
+  await page.getByRole('button', { name: 'Log in' }).click()
+
+  await expect(page.getByText("The password that you've entered is incorrect." )).toHaveCount(1);
+});
+
+test('case7 : Login with true password but have exceed spacebar at the end', async ({ page }) => {
+
+  await page.goto('https://www.facebook.com/');
+  await page.locator("xpath=//input[@id='email']").fill('main.test.automate@gmail.com')
+  await page.locator("xpath=//input[@id='pass']").fill('Password_1 ')
+  await page.getByRole('button', { name: 'Log in' }).click()
+
+  await expect(page.getByText("The password that you've entered is incorrect." )).toHaveCount(1);
+});
