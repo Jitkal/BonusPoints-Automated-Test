f217fdc0d3ce1a4d4ace66df500177b71d271cc1
Jitkal Mitrakul <66130500113@ad.sit.kmutt.ac.th>
2025-11-14 11:05:47 +0700

    test: add handle check and wait manual reslove captcha function

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -1,15 +1,23 @@
 const {test,expect}=require('@playwright/test')
 
+function captchaCheck() {
+  const captcha = page.getByText("I'm not a robot", { exact: false });
+  const isCaptchaVisible = await captcha.isVisible().catch(() => false);
+  if (isCaptchaVisible) {
+    console.log('CAPTCHA — โปรดแก้ CAPTCHA ด้วยตนเองในเบราว์เซอร์...');
+  }
+
+}
+
 test('case1 : Normal Login', async ({ page }) => {
-
   await page.goto('https://www.facebook.com/');
-  await page.locator("xpath=//input[@id='email']").fill('main.test.automate@gmail.com')
-  await page.locator("xpath=//input[@id='pass']").fill('Password_1')
-  await page.getByRole('button', { name: 'Log in' }).click()
+  await page.locator("xpath=//input[@id='email']").fill('main.test.automate@gmail.com');
+  await page.locator("xpath=//input[@id='pass']").fill('Password_1');
+  await page.getByRole('button', { name: 'Log in' }).click();
 
-  await page.goto('https://www.facebook.com/profile');
+  captchaCheck()
+  await page.waitForSelector('[aria-label="Create a post"]', { timeout: 180000 });
   await expect(page.getByRole('button', { name: 'Log in' })).toHaveCount(0);
-
 });
 
 test('case2.1 : Login with incorrect password', async ({ page }) => {
@@ -19,6 +27,8 @@ test('case2.1 : Login with incorrect password', async ({ page }) => {
   await page.locator("xpath=//input[@id='pass']").fill('Password_0')
   await page.getByRole('button', { name: 'Log in' }).click()
 
+  captchaCheck()
+  await page.waitForSelector("//div[contains(@class, 'login_form')]", { timeout: 180000 });
   await expect(page.getByText("The password that you've entered is incorrect." )).toHaveCount(1);
 });
 
@@ -29,6 +39,8 @@ test('case2.2 : Login with true password but wrong language', async ({ page }) =
   await page.locator("xpath=//input[@id='pass']").fill('ญฟหหไนพก๘ๅ')
   await page.getByRole('button', { name: 'Log in' }).click()
 
+  captchaCheck()
+  await page.waitForSelector("//div[contains(@class, 'login_form')]", { timeout: 180000 });
   await expect(page.getByText("The password that you've entered is incorrect." )).toHaveCount(1);
 });
 
@@ -40,6 +52,8 @@ test('case2.3 : Login with true password but have 1 spacebar at front', async ({
   await page.locator("xpath=//input[@id='pass']").fill(' Password_1')
   await page.getByRole('button', { name: 'Log in' }).click()
 
+  captchaCheck()
+  await page.waitForSelector("//div[contains(@class, 'login_form')]", { timeout: 180000 });
   await expect(page.getByText("The password that you've entered is incorrect." )).toHaveCount(1);
 });
 
@@ -50,6 +64,8 @@ test('case2.4 : Login with true password but have exceed spacebar at front', asy
   await page.locator("xpath=//input[@id='pass']").fill('    Password_1')
   await page.getByRole('button', { name: 'Log in' }).click()
 
+  captchaCheck()
+  await page.waitForSelector("//div[contains(@class, 'login_form')]", { timeout: 180000 });
   await expect(page.getByText("The password that you've entered is incorrect." )).toHaveCount(1);
 });
 
@@ -61,6 +77,8 @@ test('case2.5 : Login with true password but have exceed spacebar in the middle'
   await page.locator("xpath=//input[@id='pass']").fill('Pass  word_1')
   await page.getByRole('button', { name: 'Log in' }).click()
 
+  captchaCheck()
+  await page.waitForSelector("//div[contains(@class, 'login_form')]", { timeout: 180000 });
   await expect(page.getByText("The password that you've entered is incorrect." )).toHaveCount(1);
 });
 
@@ -71,6 +89,8 @@ test('case2.6 : Login with true password but have exceed spacebar at the end', a
   await page.locator("xpath=//input[@id='pass']").fill('Password_1 ')
   await page.getByRole('button', { name: 'Log in' }).click()
 
+  captchaCheck()
+  await page.waitForSelector("//div[contains(@class, 'login_form')]", { timeout: 180000 });
   await expect(page.getByText("The password that you've entered is incorrect." )).toHaveCount(1);
 });
 
